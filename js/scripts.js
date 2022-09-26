5b67af6dcdc3d70a9cd6f9912eb432e0c57d5cb7
Adejokun Oluwafunmilayo <adejokunoluwafunmilayo@gmail.com>
2022-09-26 11:05:29 -0700

    index.html

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function totalprice(price,amount){
+  return(price*amount)
+}
+
+$(document).ready(function(){
+    $("#formOne").submit(function(event){
+      event.preventDefault()
+      const inputtedProduct =$("#product").val();
+      const inputtedQuality=$("#quality").val();
+      const inputtedAmount=$("#amount").val();
+      const inputtedSeller=$("#seller").val();
+
+      const total = totalprice(inputtedAmount, inputtedQuality);
+
+      $("#display").append(
+        "<tr>" +
+          "<td>" + inputtedProduct.toUpperCase() + "</td>" +
+          "<td>" + inputtedQuality + "</td>" +
+          "<td>" + inputtedAmount + "</td>" + 
+          "<td>" + inputtedSeller.toUpperCase() + "</td>" +
+          "<td>" + total + "</td>" +
+        "</tr>"
+      )
+
+      $("#product").val(" ");
+      $("#quality").val(" ");
+      $("#amount").val(" ");
+      $("#seller").val(" ");
+
+})
+})
